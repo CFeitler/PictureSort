12e8fd735a70d028c064901e207350d0d108c740
CFeitler <c.feitler@gmx.at>
2016-10-25 18:05:48 +0200

    Add files via upload

---
+++ PictureSortDesktop/PictureSortDesktop/PictureSortDesktop/FileLocations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureSortDesktop
+{
+    public class FileLocations
+    {
+        private static FileLocations _instance;
+        public string PathUp { get; set; }
+        public string PathLeft { get; set; }
+        public string PathRight { get; set; }
+
+        private FileLocations()
+        { }
+
+        public static FileLocations Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    new FileLocations();
+                return _instance;
+            }
+        }
+    }
+}
---
+++ PictureSortDesktop/README.md
@@ -0,0 +1,2 @@
+# PictureSort
+This program helps you with sorting pictures into different folders
